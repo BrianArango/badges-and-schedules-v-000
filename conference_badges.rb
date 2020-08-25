1d9832d032695be5a98d79cab5fac05f015a26b7
Brian Arango <brianvictus@gmail.com>
2020-08-25 20:32:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,26 +1,30 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
   end
+  return nuarray
 end
 
-def assign_rooms(attendees)
-  room = 0
-  attendees.collect do |name|
-    room += 1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
   end
+  return nuarray
 end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each do |line|
-    puts line
-  end
-  assign_rooms(attendees).each do |line|
-    puts line
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  assign_rooms(array).each do |id|
+    puts id 
   end
 end
